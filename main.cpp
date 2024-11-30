5643d37e1a02999148eeacb0365cc221d12784c3
enis75079 <naveenislam55@gmail.com>
2024-11-30 08:29:19 -0800

    Initial commit

--- main.cpp
+++ main.cpp
@@ -1,9 +1,23 @@
+/*
+
+lab36: Records BST
+COMSC-210
+Naveen Islam
+
+*/
 #include <iostream>
+#include <string>
 #include "IntBinaryTree.h"
 using namespace std;
 
 int main() {
-    
+    IntBinaryTree tree;
+    int userChoice = 0;
+    string userInput = "";
+
+    while (userChoice != 5) {
+        
+    }
 
     return 0;
 }
